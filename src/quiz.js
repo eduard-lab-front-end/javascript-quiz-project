1e4511ca543ecd5a2b34c12d66003336ad06a774
eduard-lab-front-end <eduard-lab@web.de>
2024-09-02 15:14:54 +0200

    Day 1 - done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  getQuestion() {
+    let question = this.questions.find(
+      (_, index) => index == this.currentQuestionIndex
+    );
+    return question;
+  }
 
-    // 4. shuffleQuestions()
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
 
-    // 5. checkAnswer(answer)
+  shuffleQuestions() {
+    this.questions.sort(() => Math.random() - 0.5);
+  }
 
-    // 6. hasEnded()
+  checkAnswer(answer) {
+    if (answer) {
+      this.correctAnswers++;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
